3eda95c534f184866c4a5c03489fd1ad378ef8c6
lan3945663 <lan3945663@gmail.com>
2021-02-16 20:59:43 +0300

    feat: add check str using config function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = str;
+  let exitFlag = false;
+
+  function findBrackets(tempStr) {
+    bracketsConfig.forEach(el => {
+      const sConfig = el.join('');
+
+      if(tempStr.includes(sConfig)) {
+        res = tempStr.split(sConfig).join('');
+      }
+    })
+
+    if(res === tempStr) {
+      exitFlag = true;
+    }
+  }
+  
+  while(!exitFlag) {
+    findBrackets(res);
+  }
+
+  if(res === '') {
+    return true
+  } 
+
+  return false
 }
